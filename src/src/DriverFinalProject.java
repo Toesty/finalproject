5c01802a495084f15462244d83f6b3bbb9450f7c
scottrose15 <scottrose@ROOM316-20.student.rockhurst.int>
2016-05-03 10:57:40 -0500

    driver menu

--- src/src/DriverFinalProject.java
+++ src/src/DriverFinalProject.java
@@ -2,7 +2,7 @@ package src;
 import java.util.Scanner;
 public class DriverFinalProject {
 	public static void main(String[] args) {
-	
+		int menuChoice;
 		Scanner input = new Scanner(System.in);
 	
 	
@@ -10,7 +10,7 @@ public class DriverFinalProject {
 		System.out.println("1 - Create Course");
 		System.out.println("2 - Create Teacher");
 		System.out.println("3 - Create Student");
-		
+		menuChoice = input.nextInt();
 		input.close();
 	}
 }
