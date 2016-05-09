856d3482cdaf74785e3d7fa6e212c1bda8c13ded
Toesty <hoest97@gmail.com>
2016-05-09 13:52:13 -0500

    Updated driver class

--- src/DriverFinalProject.java
+++ src/DriverFinalProject.java
@@ -12,7 +12,6 @@ public class DriverFinalProject {
 		System.out.println("1 - Create Course");
 		System.out.println("2 - Create Teacher");
 		System.out.println("3 - Create Student");
-
 		System.out.println(" ");
 		System.out.println("4 - Add Student to Course");
 		System.out.println("5 - Drop Student from Course");
@@ -22,6 +21,23 @@ public class DriverFinalProject {
 
 		input.close();
 
+		if (input.nextInt() == 1) {
+			System.out.println("What would you like to name this course?");
+			}
+		if (input.nextInt() == 2) {
+			System.out.println("What is the name of this teacher?");
+		}
+		if (input.nextInt() == 3) {
+			System.out.println("What is the name of this student?");
+		}
+		if (input.nextInt() == 4) {
+			System.out.println("What course would you like to add a student to?");
+			System.out.println("What is the name of the student you would like to add to " + Course.name);
+		}
+		if (input.nextInt() == 5) {
+			System.out.println("What course would you like to add a student to?");
+			System.out.println("What is the name of the student you would like to add to " + Course.name);
 		}
 	}
+}
 
